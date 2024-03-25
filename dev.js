ac66f1134a3e2fb599fc4abc49feeddc5f80f644
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2024-03-25 20:01:32 -0300

    Atualizar o dev.js

--- dev.js
+++ dev.js
@@ -10,7 +10,7 @@ E se você está aqui é por que é um dos amantes da Literatura. <br>
 Criei essa plataforma para oferecer algo que outras plataformas não oferecem. <br>
 além de poder conhecer novas obras, você também poderá debater sobre elas em um chat de conversa. <br>
 
-Além disso, cada recomendação tem uma seção de comentários para avaliar as obras. <br>
+Além disso, cada recomendação tem uma sessão de comentários para avaliar as obras. <br>
 Eu ainda tenho muito a fazer por aqui, e queria pedir a sua ajuda e a todos os usuários que estão chegando, me ajudem a deixar isso bom para vocês. <br>
 -Caio
 
